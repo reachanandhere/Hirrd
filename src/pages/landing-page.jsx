2a3f2472c4d69b2f2f6a9033479cc3c76526e9b5
Anand Verma <anandverma552@gmail.com>
2024-08-24 01:54:17 -0400

    adding faq accordian

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import React from "react";
 import { Link } from "react-router-dom";
 import companies from "../data/companies.json";
+import faq from "../data/faq.json";
 import Autoplay from "embla-carousel-autoplay";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const LandingPage = () => {
   return (
@@ -17,36 +28,70 @@ const LandingPage = () => {
           </span>
         </h1>
 
-        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">Explore thousands of job listings or find the perfect candidate!</p>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job listings or find the perfect candidate!
+        </p>
       </section>
       <div className="flex gap-6 justify-center">
         <Link to="/jobs">
-            <Button variant='blue' size="xl">Find Jobs</Button>
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
         </Link>
         <Link to="/post-job">
-            <Button variant="destructive" size='xl'>Post a Job</Button>
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
         </Link>
       </div>
 
-      <Carousel
-       plugins={[Autoplay({delay: 2000})]}
-      className="w-full py-10"
-    >
-      <CarouselContent className="flex gap-5 sm:gap-20 items-center">
-        {
-            companies.map(({name, id, path}) => {
-                return <CarouselItem key={id} className="basis-1/3 sm:basis-1/6" >
-                    <img src={path} alt={name} className="h-9 sm:h-14 object-contain" />
-                </CarouselItem>
-            } )
-        }
-      </CarouselContent>
-      
-    </Carousel>
+      <Carousel plugins={[Autoplay({ delay: 2000 })]} className="w-full py-10">
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 sm:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className="h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
 
-        <img src="./banner.jpeg" className="w-full" />
-      <section></section>
-     
+      <img src="./banner.jpeg" className="w-full" />
+      <section className="grid grid-col-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Search and apply for Jobs, track applications and more!
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Post Jobs, manage applications, and find the best candidates!
+          </CardContent>
+        </Card>
+      </section>
+
+      <Accordion type="single" collapsible>
+        {faq.map(({ question, answer }, index) => {
+          return (
+            <AccordionItem id={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{question}</AccordionTrigger>
+              <AccordionContent>{answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+
+      </Accordion>
     </main>
   );
 };
