6da19c52a9d170653d7c2f1aa6e37bee1361334e
Anand Verma <anandverma552@gmail.com>
2024-08-23 22:52:55 -0400

    Adding component baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LandingPage from './pages/landing-page'
+import AppLayout from './layouts/app-layout'
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />
+      }
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div>Hello</div>
+    <RouterProvider router={router} /> 
   )
 }
 
