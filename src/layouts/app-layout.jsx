170137f1ca06bf02fa8ff98be336f3c29f3d70ff
Anand Verma <anandverma552@gmail.com>
2024-08-24 00:18:36 -0400

    Setting supabase with clerk

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import Header from "@/components/header";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
-        <div className='grid-background'></div>
-         <Outlet />
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-4 text-center bg-gray-800 mt-10">
+            Made with ❤️ by Anand Verma
+      </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
